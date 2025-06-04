54100312ad445bf698453c3b344c962d45ca04be
Bruno Duyé <86322+brunetton@users.noreply.github.com>
2025-06-05 00:09:04 +0200

    Add filter to only display matches that are in leass than one week

--- matchs_tv.py
+++ matchs_tv.py
@@ -1,9 +1,13 @@
 #! /usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+import datetime
+import os
+
 import lxml
 import lxml.html
 import requests
+from dotenv import load_dotenv
 
 
 def elem_content(e):
@@ -43,13 +47,44 @@ def parse_details(elems):
     return res
 
 
+def parse_date_fr(date_str):
+    months_fr = {
+        'janvier': 1, 'février': 2, 'mars': 3, 'avril': 4, 'mai': 5, 'juin': 6,
+        'juillet': 7, 'août': 8, 'septembre': 9, 'octobre': 10, 'novembre': 11, 'décembre': 12
+    }
+
+    parts = date_str.split()
+    day = int(parts[1])
+    month_name = parts[2].lower()
+    month = months_fr.get(month_name)
+    year = int(parts[3]) if len(parts) >= 4 else datetime.datetime.now().year
+
+    if month:
+        dt = datetime.datetime(year, month, day)
+        # print(dt.date())
+        return(dt)
+    else:
+        raise Exception("Unparsed month")
+
+
+def is_in_more_than_one_week(dt):
+    diff = dt - datetime.datetime.now()
+    return diff > datetime.timedelta(days=7)
+
+
 # Fetch html page
 res = requests.get("https://matchs.tv/club/real-madrid/")
 xml_tree = lxml.html.document_fromstring(res.content)
 elems = xml_tree.xpath("//div[@class='container']//table//tr")
 matches = parse_details(elems)
-print("Upcoming matches:")
+print("All matches:")
 for match in matches:
     print(f"- {match['date']} {match['hour']} — {match['teams']} — {match['competition']}")
+print("")
+filtered_matches = [match for match in matches if not is_in_more_than_one_week(parse_date_fr(match['date']))]
+if filtered_matches:
+    print("Upcoming matches (filtered):")
+    for match in filtered_matches:
+        print(f"- {match['date']} {match['hour']} — {match['teams']} — {match['competition']}")
 else:
     print("No upcoming matches within one week.")
